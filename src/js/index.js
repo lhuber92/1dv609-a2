3c031160532901e3d969c263d9970900f6896b63
Leopold Huber <lh223sf@student.lnu.se>
2021-12-09 16:02:28 +0100

    fixes

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
 // Disable eslint check (have to do like this for materializecss to work)
 /*eslint-disable */
 // Initializes all of the Materialize Components
+import "materialize-css/dist/css/materialize.css";
+import "materialize-css/dist/js/materialize.js";
+
 document.addEventListener('DOMContentLoaded', function () {
   // Sidenav:
   const elems = document.querySelectorAll('.sidenav')
